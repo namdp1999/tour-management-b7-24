639357b3f4fc89b6d4bed1341d8319d7d50fd996
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-12-02 19:58:52 +0700

    2.1 + 2.2

---
+++ controllers/client/tour.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import Tour from "../../models/tour.model";
+
+export const index = async (req: Request, res: Response) => {
+  // SELECT * FROM tours WHERE deleted = false AND status = "active";
+  const tours = await Tour.findAll({
+    where: {
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  });
+
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours
+  });
+}
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import sequelize from "./config/database";
 sequelize;
 
-import Tour from "./models/tour.model";
+import { routesClient } from "./routes/client/index.route";
 
 const app: Express = express();
 const port: number = 3000;
@@ -12,21 +12,7 @@ const port: number = 3000;
 app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views
 app.set('view engine', 'pug'); // template engine sử dụng: pug
 
-app.get("/tours", async (req: Request, res: Response) => {
-  // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  });
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+routesClient(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
