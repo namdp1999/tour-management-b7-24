484198f9e191ea0f23ccbd8fd8765c71f41c4d1e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-12-02 20:24:10 +0700

    3. Làm trang Danh mục tour (bên client)

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,8 +1,11 @@
 import { Express } from "express";
 import { toursRoute } from "./tour.route";
+import { categoriesRoute } from "./category.route";
 
 export const routesClient = (app: Express) => {
 
   app.use("/tours", toursRoute);
+
+  app.use("/categories", categoriesRoute);
 
 }
