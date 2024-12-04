a7fbd3cde186834280869e091acc1e58028e7953
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-12-04 19:42:05 +0700

    3.1. Làm giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,11 +1,14 @@
 import { Express } from "express";
 import { toursRoute } from "./tour.route";
 import { categoriesRoute } from "./category.route";
+import { cartRoute } from "./cart.route";
 
 export const routesClient = (app: Express) => {
 
   app.use("/tours", toursRoute);
 
   app.use("/categories", categoriesRoute);
+
+  app.use("/cart", cartRoute);
 
 }
