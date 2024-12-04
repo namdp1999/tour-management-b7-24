cb2a8bd484f0a3092ab1e42488f40a6eb05d010e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-12-04 19:09:17 +0700

    1.3. Hiển thị ảnh dạng slider

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// swiper-images-main
+new Swiper('.swiper-images-main', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// End swiper-images-main
